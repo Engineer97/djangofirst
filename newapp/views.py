9d9b9fc1f217f9c0521288576f979329215f2378
engineer97 <dbgmlwo1997@naver.com>
2024-01-26 17:32:03 +0900

    오늘은 칼퇴

--- newapp/views.py
+++ newapp/views.py
@@ -39,5 +39,12 @@ def booksAPI(request):
 
 
 
+@api_view(['GET'])
+def oneBookAPI(request, bid):
+        #Book 테이블에서 bid 컬럼의 값이 bid 인 값을 찾아옵니다.
+        book = get_object_or_404(Book, bid=bid)
+        #출력할 수 있도록 변환
+        serializer = BookSerializer(book)
+        return Response(serializer.data)
 
 
