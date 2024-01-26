846a96d30bc085303231795f193b593542bf26df
engineer97 <dbgmlwo1997@naver.com>
2024-01-26 17:07:21 +0900

    교보문고 만들기

--- newworldproject/urls.py
+++ newworldproject/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+# url의 처리를 다른 모듈에게 위임하고자 할 때 사용하는 패키지를 import
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("example/", include("newapp.urls")), # 이 문장은 example로 시작하는 요청이 오면 newapp 의 urls.py 파일에 처리를 위임
 ]
